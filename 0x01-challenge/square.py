42fb2930bcf1763f21f509966eb2084109c9e169
akakiplimo <akakiplimo@gmail.com>
2022-10-10 06:13:42 -0700

    revert

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Module that contains a Square Class """
 
 
-class Square():
+class square():
     """ Square class definition """
     
     width = 0
@@ -28,7 +28,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
